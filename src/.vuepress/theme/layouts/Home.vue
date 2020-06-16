6b4c0d7c9fd2081607b5e7bb8df353c1c28265a9
Manabu TERADA <terada@cmscom.jp>
2020-06-17 00:04:58 +0900

    modify breakpoint

--- src/.vuepress/theme/layouts/Home.vue
+++ src/.vuepress/theme/layouts/Home.vue
@@ -2,15 +2,15 @@
   <v-container>
     <v-row>
       <v-col>
-        <h1>terapyon channel podcast Top page</h1>
+        <h1 :class="$vuetify.breakpoint.mdAndUp? 'left': 'center'">terapyon channel podcast Top page</h1>
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="7">
+      <v-col cols="12" md="7">
         <v-img class="main-image" src="/terada-uspycon2019.jpg" />
       </v-col>
-      <v-col cols="5">
-        <v-card shaped>
+      <v-col cols="12" md="5">
+        <v-card :shaped="true">
           <v-card-text>
             <div>ここは Manabu TERADA が個人で行っている Podcast の Web サイトです。</div>
             <p class="display-1 text--primary">Podcastサービス</p>
@@ -32,6 +32,10 @@
 <style scoped>
 h1 {
   font-size: 3em;
+}
+h1.center {
+  text-align: center;
+  font-size: 2em;
 }
 .display-1 {
   font-size: 0.7em;
