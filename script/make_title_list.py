e255b9906c3e7876692fe8a68eb1f4cbfe61e4c1
Manabu TERADA <terada@cmscom.jp>
2025-01-09 15:53:42 +0900

    105

--- script/make_title_list.py
+++ script/make_title_list.py
@@ -16,6 +16,13 @@ def get_attr(item: dict[str, str | None], key: str) -> str | None:
     return item.get(key, "?")
 
 
+def get_audio(item: dict[str, str | None]) -> tuple[str | None, str | None]:
+    for link in item.links:
+        if link.rel == "enclosure":
+            return link.length, link.href
+    return None, None
+
+
 if __name__ == "__main__":
     import sys
     args = sys.argv
@@ -30,5 +37,6 @@ if __name__ == "__main__":
             title = get_title(item)
             date = get_attr(item, "published")
             episode = get_attr(item, "itunes_episode")
-            f.write(f"{episode},{date},{title}")
+            length, audio = get_audio(item)
+            f.write(f"{episode},{date},{length},{audio},{title}")
             f.write("\n")
